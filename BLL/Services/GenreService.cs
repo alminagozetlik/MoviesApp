6a296e59c1573647751b4b5e6c06ff5c4d8f6893
almina gözetlik <almina2308@gmail.com>
2024-12-25 23:45:34 +0300

    genres added

--- BLL/Services/GenreService.cs
+++ BLL/Services/GenreService.cs
@@ -1,6 +1,7 @@
 ﻿using BLL.DAL;
 using BLL.Models;
 using BLL.Services.Bases;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +18,22 @@ namespace BLL.Services
 
         public ServiceBase Create(Genre record)
         {
-            throw new NotImplementedException();
+            if (_db.Genres.Any(p => p.Id != record.Id && p.Name.ToLower() == record.Name.ToLower().Trim()))
+                return Error("Genre with the same name and surname and  retired situation exists");
+            record.Name = record.Name?.Trim();
+            _db.Genres.Add(record);
+            _db.SaveChanges();
+            return Success("Genre created successfully.");
         }
 
         public ServiceBase Delete(int id)
         {
-            throw new NotImplementedException();
+            var entity = _db.Genres.Include(p => p.MovieGenres).SingleOrDefault(p => p.Id == id);
+            if (entity is not null && entity.MovieGenres.Any())
+                return Error("Genre can't be deleted because it has relational entities!");
+            _db.Genres.Remove(entity);
+            _db.SaveChanges();
+            return Success("Genre deleted successfully.");
         }
 
         public IQueryable<GenreModel> Query()
@@ -32,7 +43,15 @@ namespace BLL.Services
 
         public ServiceBase Update(Genre record)
         {
-            throw new NotImplementedException();
+            if (_db.Genres.Any(p => p.Id != record.Id && p.Name.ToLower() == record.Name.ToLower().Trim()))
+                return Error("Genre with the same name exists!");
+            var entity = _db.Genres.SingleOrDefault(s => s.Id == record.Id);
+            if (entity is null)
+                return Error("Genre can't be found!");
+            entity.Name = record.Name?.Trim();
+            _db.Genres.Update(entity);
+            _db.SaveChanges();
+            return Success("Genre updated successfully.");
         }
     }
 }
